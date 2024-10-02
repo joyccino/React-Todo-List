9d3cb5ee297a6a6aa13769f4f82cd9610ef8a63f
Joyccino <67300266+joyccino@users.noreply.github.com>
2024-10-02 16:01:25 +0900

    props 테스트

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   const subject = 'React';
+  console.log(props);
   return (
     <div className="App">
       <header className="App-header">
