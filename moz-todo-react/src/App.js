ccc2c6f1247399a38d7a499f06663155b21a9d7a
Joyccino <67300266+joyccino@users.noreply.github.com>
2024-10-02 15:55:19 +0900

    변수 사용하기

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -3,11 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const subject = 'React';
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>"Hello, World!"</p>
+        <p>"Hello, {subject}!"</p>
       </header>
     </div>
   );
