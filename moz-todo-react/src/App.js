6cbc702982e6d1df0c7e6c280d5be5581a5590c2
Joyccino <67300266+joyccino@users.noreply.github.com>
2024-10-02 16:11:58 +0900

    필요하지 않은 파일들 삭제

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from './logo.svg';
 
 function App(props) {
   const subject = props.subject;
@@ -7,7 +8,6 @@ function App(props) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>"Hello, {subject}!"</p>
       </header>
     </div>
